b26d7b5b9e1d57b323883deafbbb65e6ec3491a1
Guillermo Rodas <me@guillermorodas.com>
2022-05-19 23:12:21 +0200

    Avances en el Autocomplete

--- components/Autocomplete.js
+++ components/Autocomplete.js
@@ -1,23 +1,65 @@
 import React, { useEffect, useRef, useState } from "react";
 import Tag from "./Tag";
 
+const pluralize = (length, word) => {
+  return length === 1 ? word : `${word}s`;
+};
+
 const Autocomplete = ({ options, values, setValues }) => {
   const [isEditable, setIsEditable] = useState(false);
-  const fileteredOptions = options.filter((option) => !values.includes(option));
+  const [filter, setFilter] = useState("");
+  const inputRef = useRef(null);
+
+  // TODO: Think about a better name to avoid confusions with the filter, setFilter
+  const fileteredOptions = options
+    .filter((option) => !values.includes(option))
+    .filter((option) => {
+      if (filter?.length) {
+        return option.includes(filter);
+      }
+      return option;
+    });
 
   useEffect(() => {
-    const closeOptions = () => setIsEditable(false);
+    const closeOptions = () => {
+      setIsEditable(false);
+      setFilter("");
+    };
     window.addEventListener("click", closeOptions);
     return () => window.removeEventListener("click", closeOptions);
   }, []);
 
+  const hasValues = !!values.length;
+  const [firstTag, secondTag, ...tags] = values;
+  const summaryValues = [
+    firstTag,
+    secondTag,
+    !!tags.length && `${tags.length} more ${pluralize(tags.length, "tag")}`,
+  ];
+
   if (isEditable) {
     return (
       <>
-        <div className="container">
+        <div
+          className="container"
+          onClick={(event) => {
+            event.stopPropagation();
+            inputRef.current.focus();
+          }}
+        >
           {!!values?.length &&
-            values.map((value) => <Tag key={value}>{value}</Tag>)}
-          <input list="tags" type="text" autoFocus placeholder="Search..." />
+            summaryValues
+              .filter(Boolean)
+              .map((value) => <Tag key={value}>{value}</Tag>)}
+          <input
+            ref={inputRef}
+            list="tags"
+            type="text"
+            autoFocus
+            placeholder={!hasValues && "Search..."}
+            value={filter}
+            onChange={(event) => setFilter(event.currentTarget.value)}
+          />
           {!!fileteredOptions?.length && (
             <div className="options">
               {fileteredOptions.map((item) => (
@@ -26,7 +68,8 @@ const Autocomplete = ({ options, values, setValues }) => {
                   onClick={(event) => {
                     event.stopPropagation();
                     setValues([...new Set([...values, item])]);
-                    setIsEditable(false);
+                    setFilter("")
+                    inputRef.current.focus()
                   }}
                 >
                   {item}
@@ -41,19 +84,29 @@ const Autocomplete = ({ options, values, setValues }) => {
             display: inline-flex;
             flex-wrap: wrap;
             gap: 4px;
+            height: 28px;
             align-items: center;
             min-width: 200px;
             max-width: 400px;
             background: white;
+            border: 1px solid white;
+            outline: 1px auto white;
+            border-radius: 4px;
+          }
+
+          .container:focus-within {
+            outline: 1px auto #e11d48;
           }
 
           .container > input {
-            height: 28px;
+            height: 100%;
             width: auto;
-            border-radius: 4px;
             border: none;
             padding: 0 8px;
             font-size: 16px;
+            width: 100px;
+            outline: none;
+            border: 2px dashed slateblue;
           }
 
           .options {
@@ -97,7 +150,9 @@ const Autocomplete = ({ options, values, setValues }) => {
         }}
       >
         {!!values?.length
-          ? values.map((value) => <Tag key={value}>{value}</Tag>)
+          ? summaryValues
+              .filter(Boolean)
+              .map((value) => <Tag key={value}>{value}</Tag>)
           : "Search..."}
       </div>
       <style jsx>{`
@@ -111,9 +166,9 @@ const Autocomplete = ({ options, values, setValues }) => {
           padding: 0 8px;
           min-width: 180px;
           color: #2d0612;
-          min-height: 28px;
+          height: 28px;
           font-size: 16px;
-          border: none;
+          border: 1px solid white;
           outline: 1px auto white;
           min-width: 200px;
           max-width: 400px;
