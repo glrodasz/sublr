bcee4e405eb4f1c7d132130428a20211b996e2ce
Guillermo Rodas <me@guillermorodas.com>
2023-02-12 00:17:38 +0100

    Fix issue

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,14 +1,12 @@
+const fs = require("fs");
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
-  let serviceAccountKey = fs.readFileSync("./serviceAccountKey.dev.json")
+  const env = process.env.NODE_ENV === "production" ? "prod" : "dev";
+  const serviceAccountKey = require(`./serviceAccountKey.${env}.json`);
 
-  if(process.env.NODE_ENV === 'production') {
-    serviceAccountKey = fs.readFileSync("./serviceAccountKey.prod.json")
-  }
-  
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccountKey),
-    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`,
+    databaseURL: `https://${serviceAccountKey.project_id}.firebaseio.com`,
   });
 }
