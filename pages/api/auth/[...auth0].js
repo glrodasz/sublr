db849d9b426f987793d26cdad21f7953df81e895
Guillermo Rodas <guillermo.rodas@eqtpartners.com>
2023-12-06 16:30:02 +0100

    Update env variable

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,5 +1,5 @@
 import { handleAuth } from "@auth0/nextjs-auth0";
 
 export default handleAuth({
-  baseUrl: process.env.AUTH0_BASE_URL || process.env.VERCEL_URL,
+  baseUrl: process.env.AUTH0_BASE_URL || `https://${process.env.VERCEL_URL}`,
 });
